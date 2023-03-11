a2deaf5fcc8a69892a9ca447284fc606a1c82814
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2023-03-11 16:39:47 +0700

    [tienhuynh-tn] Cors Config

---
+++ src/main/java/com/happy3friends/toiletmapbackend/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.happy3friends.toiletmapbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.OPTIONS.name(), HttpMethod.DELETE.name())
+                        .allowedHeaders("*")
+                        .allowedOrigins("http://localhost:8000",
+                                "http://localhost:8081",
+                                "http://localhost:3000");
+            }
+        };
+    }
+}
