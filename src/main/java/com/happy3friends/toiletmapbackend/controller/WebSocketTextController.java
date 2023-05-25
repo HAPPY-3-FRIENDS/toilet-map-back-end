f57c6906c0084cb35ba04fae597873c04d1c8cb6
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2023-05-26 01:14:22 +0700

    [tienhuynh-tn] web-socket api tien

--- src/main/java/com/happy3friends/toiletmapbackend/controller/WebSocketTextController.java
+++ src/main/java/com/happy3friends/toiletmapbackend/controller/WebSocketTextController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.RolesAllowed;
 
-@RestController(value = "/api/test")
+@RestController(value = "/api/tien")
 public class WebSocketTextController {
 
     @Autowired
