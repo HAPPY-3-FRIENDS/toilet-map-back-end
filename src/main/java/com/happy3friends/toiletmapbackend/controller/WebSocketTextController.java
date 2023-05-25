0f0ad7e23dcff3acfb69dfc00509ae378d90a0ac
hieutk-se <hieutk.se@gmail.com>
2023-05-26 00:23:27 +0700

    [HieuTK2] Demo WebSocket

---
+++ src/main/java/com/happy3friends/toiletmapbackend/controller/WebSocketTextController.java
@@ -0,0 +1,43 @@
+package com.happy3friends.toiletmapbackend.controller;
+
+import com.happy3friends.toiletmapbackend.config.OpenApiConfig;
+import com.happy3friends.toiletmapbackend.constant.RoleConstant;
+import com.happy3friends.toiletmapbackend.dto.TextMessageDTO;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
+
+@RestController
+public class WebSocketTextController {
+
+    @Autowired
+    SimpMessagingTemplate template;
+
+    @SecurityRequirement(name = OpenApiConfig.securitySchemeName)
+    @RolesAllowed({RoleConstant.TOILET})
+    @PostMapping("/send")
+    public ResponseEntity<Void> sendMessage(@RequestBody TextMessageDTO textMessageDTO) {
+        template.convertAndSend("/topic/message", textMessageDTO);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @MessageMapping("/sendMessage")
+    public void receiveMessage(@Payload TextMessageDTO textMessageDTO) {
+        // receive message from client
+    }
+
+    @SendTo("/topic/message")
+    public TextMessageDTO broadcastMessage(@Payload TextMessageDTO textMessageDTO) {
+        return textMessageDTO;
+    }
+}
---
+++ src/main/java/com/happy3friends/toiletmapbackend/dto/TextMessageDTO.java
@@ -0,0 +1,15 @@
+package com.happy3friends.toiletmapbackend.dto;
+
+public class TextMessageDTO {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
