8e6fea375076ad846a0bccc66d61cd2d499347bc
hieutk-se <hieutk.se@gmail.com>
2023-06-02 01:08:36 +0700

    [HieuTK2] Announcement Entity

---
+++ src/main/java/com/happy3friends/toiletmapbackend/entity/AnnouncementEntity.java
@@ -0,0 +1,48 @@
+package com.happy3friends.toiletmapbackend.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Table(name = "Announcement", schema = "dbo", catalog = "ToiletMap")
+public class AnnouncementEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "Id", nullable = false)
+    private int id;
+
+    @Basic
+    @Column(name = "Title", nullable = false)
+    private String title;
+
+    @Basic
+    @Column(name = "Url", nullable = false, length = -1)
+    private String url;
+
+    @Basic
+    @Column(name = "ImageSource", nullable = false, length = -1)
+    private String imageSource;
+
+    @Basic
+    @Column(name = "StartDate", nullable = false)
+    private Date startDate;
+
+    @Basic
+    @Column(name = "EndDate", nullable = false)
+    private Date endDate;
+
+    @Basic
+    @Column(name = "Description", nullable = false, length = -1)
+    private String description;
+
+    @Basic
+    @Column(name = "Type", nullable = false, length = 20)
+    private String type;
+}
