8c2da93a786cb620d2bf9dbbcffcecda91abf61e
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2023-06-30 00:36:38 +0700

    [tienhuynh-tn] fix bug get list toilets by company id with suggestion

--- src/main/java/com/happy3friends/toiletmapbackend/repository/SuggestionRepository.java
+++ src/main/java/com/happy3friends/toiletmapbackend/repository/SuggestionRepository.java
@@ -27,12 +27,6 @@ public interface SuggestionRepository extends JpaRepository<SuggestionEntity, In
             "              ON t.CompanyId = c.Id " +
             "         JOIN Suggestion sg " +
             "              ON t.Id = sg.ToiletId " +
-            "                  AND ((sg.StartDate = DATEADD(QUARTER, DATEDIFF(QUARTER, 0, GETDATE()) - 2, 0) AND " +
-            "                        sg.EndDate = DATEADD(QUARTER, DATEDIFF(QUARTER, 0, GETDATE()) - 1, -1)) " +
-            "                      OR " +
-            "                       (sg.StartDate = DATEADD(QUARTER, DATEDIFF(QUARTER, 0, GETDATE()) - 1, 0) AND " +
-            "                        sg.EndDate = DATEADD(QUARTER, DATEDIFF(QUARTER, 0, GETDATE()), -1)) " +
-            "                     ) " +
             "WHERE c.Id = :companyId " +
             "  AND sg.ToiletId IN (:listToiletIds) " +
             "ORDER BY t.Id ASC, sg.Streak DESC", nativeQuery = true)
