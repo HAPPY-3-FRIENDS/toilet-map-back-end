4d40778f3b49e0e6bf4903e69dcf9af290e669d0
hieutk-se <hieutk.se@gmail.com>
2023-07-17 15:58:37 +0700

    [HieuTK2] fix script

--- src/main/java/com/happy3friends/toiletmapbackend/service/serviceImpl/ScriptServiceImpl.java
+++ src/main/java/com/happy3friends/toiletmapbackend/service/serviceImpl/ScriptServiceImpl.java
@@ -140,7 +140,7 @@ public class ScriptServiceImpl implements ScriptService {
 
     private String initDate() {
         Date now = DateTimeUtil.getDateNow();
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return dateFormat.format(now);
     }
 }
