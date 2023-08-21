10c9323ab8fcc6e06e40fee41c569d523637650c
hieutk-se <hieutk.se@gmail.com>
2023-08-21 20:30:35 +0700

    [HieuTK2] Change Configuration entity

--- src/main/java/com/happy3friends/toiletmapbackend/entity/ConfigurationEntity.java
+++ src/main/java/com/happy3friends/toiletmapbackend/entity/ConfigurationEntity.java
@@ -13,14 +13,10 @@ import java.util.Objects;
 @EqualsAndHashCode
 @Table(name = "Configuration", schema = "dbo", catalog = "ToiletMap_Final_Final")
 public class ConfigurationEntity {
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-    @Column(name = "Id", nullable = false)
-    private int id;
-    @Basic
-    @Column(name = "Name", nullable = false, length = 50)
-    private String name;
+    @Column(name = "Key", nullable = false)
+    private String key;
     @Basic
     @Column(name = "Value", nullable = false, length = 50)
-    private String value;
+    private int value;
 }
